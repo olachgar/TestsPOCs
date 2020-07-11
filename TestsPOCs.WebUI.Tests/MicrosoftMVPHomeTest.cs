9b2dfdc1bf7104927fdc876ca0c5b5d56d78898b
Omar LACHGAR <olachgar@gmail.com>
2020-07-11 17:43:36 +0200

    feat : add selenium & first test with chrom driver

---
+++ TestsPOCs.WebUI.Tests/MicrosoftMVPHomeTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace TestsPOCs.WebUI.Tests
+{
+    [TestFixture]
+    public class MicrosoftMVPHomeTest : TestBase
+    {
+        [SetUp]
+        public void setup()
+        {
+
+        }
+
+        [Test]
+        public void test()
+        {
+            webDriver.Manage().Window.Maximize();
+            webDriver.Url = "https://www.google.com/";
+
+            webDriver.Navigate();
+
+            Assert.AreEqual("Google kaka", webDriver.Title);
+        }
+    }
+}
---
+++ TestsPOCs.WebUI.Tests/TestBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Common;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestsPOCs.WebUI.Tests
+{
+    public class TestBase : IDisposable
+    {
+        protected readonly IWebDriver webDriver;
+
+        public TestBase()
+        {
+            webDriver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            webDriver.Close();
+        }
+    }
+}
